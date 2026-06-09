9a9ade10e82bd77122021d6ab7ff5f23ac3a73e2
daae <kda0327@gmail.com>
2026-06-09 15:20:17 +0900

    테스트

---
+++ book-backend/src/main/java/com/aivle12/book_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.aivle12.book_backend.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
